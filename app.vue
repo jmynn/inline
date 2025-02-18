e4ae55980a866434ae8c5a81d36aa8028a090f46
jmynn <poldenv1975@gmail.com>
2025-02-18 15:11:57 +0400

    create base template for project, add assets, add icons, add nuxtimage, add font, create base styles, add stylekit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+<style>
+  @import './styles/nullstyle.css';
+  @import './styles/stylekit.css';
+  @import './styles/globals.css';
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useHead({
+	title: 'Pizzashop',
+	description: 'This is a home page for SEO',
+});
+</script>
+
+<template>
+	<ViewsMain />
+</template>
+
+<style scoped></style>
